3e9c84715824f00653518a5153da6f5064fa7acb
alingen <kot.90313@gmail.com>
2022-11-29 11:01:55 +0900

    画像アップロード機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get 'comments/:id/show' => 'comments#show'
+  get 'comments/:id' => 'comments#show'
   get 'comments/:id/new' => 'comments#new'
   post 'comments/:id/create' => 'comments#create'
 
   post 'users/:id/update' => 'users#update'
 
-  devise_for :users
   get '/' => 'home#top'
+
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   patch '/users/:id/withdrawal' => 'users#withdrawal', as: 'withdrawal'
-  get 'tweets/new' => 'tweets#new'
-  post 'tweets/create' => 'tweets#create'
 
   resources :users, only: %i[show]
+  resources :tweets
 end
